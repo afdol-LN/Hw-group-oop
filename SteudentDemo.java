c1c9ccc93ea32f403265e3b3cd2093209918c779
MongckonD <123mongckontham@gmail.com>
2025-07-31 22:45:57 +0700

    gender+nickname

--- SteudentDemo.java
+++ SteudentDemo.java
@@ -1,8 +1,12 @@
-public class SteudentDemo {
+public class SteudentDemo extends People{
     //hello
     //hello
-    static void main(String[] args){
+    public static void main(String[] args){
 
+        People s1 = new People("Male", "afdol");
+
+        System.out.println("Gender: " + s1.getGender());
+        System.out.println("Nickname: " + s1.getNickname());
         
     }
 }
